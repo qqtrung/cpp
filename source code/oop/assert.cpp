450dc7c160f209f7c775f808cdd760841450a591
Nq. Trung <nqtrung21112005@gmail.com>
2024-07-22 19:17:40 +0700

    debug cp
    competitive programing

---
+++ source code/oop/assert.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<assert.h> 
+using namespace std;
+
+int main()
+{
+    
+    int x = 8; 
+    x = 7; 
+    assert(x == 7); 
+    cout << "login successfully"; 
+    
+    
+    return 0;
+}
