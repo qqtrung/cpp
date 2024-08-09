5651b12451de22be59581525497fa6bd1f29a13a
Nq. Trung <144644664+qqtrung@users.noreply.github.com>
2024-08-09 21:21:32 +0700

    Update ex0000.cpp

--- source code/cp/ex0000.cpp
+++ source code/cp/ex0000.cpp
@@ -1,32 +1,36 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <map>
+#include <vector>
+#include <cstdint>
+#include <algorithm>
+
 using namespace std; 
-typedef long long ll; 
+using int64 = int64_t; 
+using ii = pair<int, int>; 
+#define sz(v) ((int) v.size())
+#define all(v) (v).begin(), (v).end() 
 
-void solve() 
-{
-    // solve
 
+size_t DataType(int64 largeNumber) {
+    return largeNumber; 
 }
 
-int main() 
-{
-    auto start = chrono::high_resolution_clock::now(); 
-    cin.tie(0); 
+int main() {
+
     ios_base::sync_with_stdio(false); 
+    cin.tie(nullptr); 
+    cout.tie(nullptr); 
+
+    int64 largeNumber = 9223372036854775807; 
+    cout << DataType(largeNumber) << "\n"; 
     
-    ifstream in("input.txt"); 
-    cin.rdbuf(in.rdbuf()); 
+    vector<int> a = {4, 2, 3, 4, 1, 4}; 
+    a.emplace_back(10); // faster push_back 
 
-    ofstream out("output.txt"); 
-    cout.rdbuf(out.rdbuf()); 
+    sort(all(a)); 
+    // for (int i = 0; i < sz(a); i++) {
+    //     cout << a[i] << " "; 
+    // }
 
-    int st;
-    cin >> st; 
-    while(st--)
-        solve(); 
-
-    auto stop = chrono::high_resolution_clock::now(); 
-    auto duration = chrono::duration_cast<chrono::milliseconds>(stop - start); 
-    cerr << "Time taken by funtion: " << duration.count() << " miliseconds"; 
-
+    return 0;
 }
